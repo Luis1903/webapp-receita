8fa27e82fb6024c405938249173b39404368344f
Luis1903 <santiagofelipe1903@gmail.com>
2023-08-03 14:52:11 -0300

    página index adicionada

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'cabecalho.php';
+
+?>
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <div class="col-8">
+                    <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://images.pexels.com/photos/8908609/pexels-photo-8908609.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://images.pexels.com/photos/8530995/pexels-photo-8530995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://images.pexels.com/photos/6947255/pexels-photo-6947255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
+                </div>
+            </div>
+             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+             </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+             </button>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'rodape.php'; ?>
